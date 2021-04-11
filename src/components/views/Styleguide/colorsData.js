923b25f439949fa0631cdf2fb55e938c0eac5eab
kherma <kamil.herma01@gmail.com>
2021-04-12 00:04:14 +0200

    Update styleguide

--- src/components/views/Styleguide/colorsData.js
+++ src/components/views/Styleguide/colorsData.js
@@ -2,14 +2,14 @@ import styles from './Styleguide.module.scss';
 
 const data = [
   {
-    itemClass: styles.colorDark,
-    hex: '#000000',
-    varName: '$dark',
-  },
-  {
     itemClass: styles.colorLight,
     hex: '#ffffff',
     varName: '$light',
+  },
+  {
+    itemClass: styles.colorGreyLight1,
+    hex: '#f4f4f4',
+    varName: '$grey-light-1',
   },
   {
     itemClass: styles.colorGrey,
@@ -20,6 +20,26 @@ const data = [
     itemClass: styles.colorGreyDark1,
     hex: '#777777',
     varName: '$grey-dark-1',
+  },
+  {
+    itemClass: styles.colorGreyDark2,
+    hex: '#333333',
+    varName: '$grey-dark-2',
+  },
+  {
+    itemClass: styles.colorGreyDark3,
+    hex: '#222222',
+    varName: '$grey-dark-3',
+  },
+  {
+    itemClass: styles.colorGreyDark4,
+    hex: '#111111',
+    varName: '$grey-dark-4',
+  },
+  {
+    itemClass: styles.colorDark,
+    hex: '#000000',
+    varName: '$dark',
   },
   {
     itemClass: styles.colorPurple,
